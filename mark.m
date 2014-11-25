0c4f093945f36d93528b6dceb76b2b65e07dab2f
Nils Fischer <n.fischer@viwid.com>
2014-11-25 14:52:08 +0100

    cleaned repo

--- mark.m
+++ mark.m
@@ -1,18 +1,10 @@
-function mat_out = mark( Image_in )
-% Shows an Image and lets you define 4 Positions
-%   
-    imshow(Image_in);
+function p = mark(I)
+    % display image I in figure
+    imshow(I);
+    
+    % get the position of the 4 markers
     p = ginput(4);
     
-    % Mitteln 1->2 = 3->4 = (1->2 + 3->4) / 2, (2->3 + 4->1) / 2:
-    
-    %p_mean = p;
-    %length1 = (sqrt((p(1,1)-p(2,1))^2+(p(1,2)-p(2,2))^2)+sqrt((p(3,1)-p(4,1))^2+(p(3,2)-p(4,2))^2))/2;
-    %length2 = (sqrt((p(2,1)-p(3,1))^2+(p(2,2)-p(3,2))^2)+sqrt((p(4,1)-p(1,1))^2+(p(4,2)-p(1,2))^2))/2;
-    %length1
-    %length2
-    mat_out = p;
-    
-
+    % close the figure
+    close;
 end
-
