6ce15a15c46bf030add0f9db7b186494471443e0
svenzel <sven1@pfalzabc.de>
2015-01-11 01:35:18 +0100

    sa abend commit

--- find_marks.m
+++ find_marks.m
@@ -1,4 +1,4 @@
-function p = find_marks( Image )
+function p = find_marks( Image, landscape...perfilename )
 % Function to determine the position of the 4 blue marks
 % Warning: Currently only works, if 4 connected components are found
 % Could be improved by closing holes
@@ -16,9 +16,9 @@ function p = find_marks( Image )
 
         %Close Holes
 
-        %SE = strel('disk', 10, 6);
-        %image_bin = imdilate(image_bin, SE);
-        %image_bin = imerode(image_bin, SE);
+        SE = strel('disk', 10, 6);
+        image_bin = imdilate(image_bin, SE);
+        image_bin = imerode(image_bin, SE);
    
     
         %Find connected components and determine the center
@@ -29,7 +29,7 @@ function p = find_marks( Image )
         [count, ~] = size(p);
         threshold = threshold +1;
     end
-    imshow(image_bin);
+    
     if count ~= 4
         p = 0;
         return
@@ -48,13 +48,28 @@ function p = find_marks( Image )
     if ( p(4, 1) > p(3, 1) )
         p = p([1,2,4,3], :);
     end
-        
+    
+    % Vektoren
+    v12 = p(2,:)-p(1,:);
+    v23 = p(3,:)-p(2,:);
+    v34 = p(4,:)-p(3,:);
+    v41 = p(1,:)-p(4,:);
+      
+    % Längen
+    distance12 = sqrt((p(1,1) - p(2,1)).^2 + (p(1,2) - p(2,2)).^2);
+    distance23 = sqrt((p(2,1) - p(3,1)).^2 + (p(2,2) - p(3,2)).^2);
+    distance34 = sqrt((p(3,1) - p(4,1)).^2 + (p(3,2) - p(4,2)).^2);
+    distance41 = sqrt((p(1,1) - p(4,1)).^2 + (p(1,2) - p(4,2)).^2);
+    
+    % Winkel
+    arc1 = abs(acos((v12*(-v41'))/(distance12*distance41))/3.14*180);
+    arc2 = abs(acos((v23*(-v12'))/(distance12*distance23))/3.14*180);
+    arc3 = abs(acos((v34*(-v23'))/(distance23*distance34))/3.14*180);
+    arc4 = abs(acos((v41*(-v34'))/(distance34*distance41))/3.14*180);
+    
     % Prüfe nach Hochformat/Querformat:
-    distance12 = (p(1,1) - p(2,1)).^2 + (p(1,2) - p(2,2)).^2;
-    distance23 = (p(2,1) - p(3,1)).^2 + (p(2,2) - p(3,2)).^2;
-    distance34 = (p(3,1) - p(4,1)).^2 + (p(3,2) - p(4,2)).^2;
-    distance41 = (p(1,1) - p(4,1)).^2 + (p(1,2) - p(4,2)).^2;
-    if distance12 < distance23
+    %%if distance12 < distance41
+    if (distance12 < distance41) || (abs(arc1-arc3)+abs(arc2-arc4))>40
         p = p([4,1,2,3], :);    % Permutation, falls p im Hochformat vorliegt
     end
     
